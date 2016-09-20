f3175ce0697577c9fa76ab2ffcc5b40c44e119dd
MaxDev <maxdev@smartools.co.kr>
2016-09-20 20:34:10 +0900

    팝업창 출력 지원

--- theme/st-basic/index.php
+++ theme/st-basic/index.php
@@ -3,6 +3,8 @@ define('_INDEX_', true);
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 include_once(G5_THEME_PATH.'/head.php');
+
+is_mobile()
 ?>
 
 
@@ -11,16 +13,22 @@ include_once(G5_THEME_PATH.'/head.php');
 	if( !$st_layout_main ) {
 ?>
 	<div class="content">
+		<?php include (is_mobile()? G5_MOBILE_PATH: G5_BBS_PATH).'/newwin.inc.php'; // 팝업레이어 (index에서만 실행)?>		
+	
 		<?php include_once G5_THEME_PATH.'/_inc/slider.php'?>
 		<?php include_once G5_THEME_PATH.'/_inc/main.php'?>
 	</div>		
 	<?php } elseif( $st_layout_main==1 ) { ?>
 	<div class="container content">
+		<?php include (is_mobile()? G5_MOBILE_PATH: G5_BBS_PATH).'/newwin.inc.php'; // 팝업레이어 (index에서만 실행)?>		
+		
 		<?php include_once G5_THEME_PATH.'/_inc/slider.php'?>
 		<?php include_once G5_THEME_PATH.'/_inc/main.php'?>
 	</div>		
 	<?php } else { ?>
 	<div class="container content">
+		<?php include (is_mobile()? G5_MOBILE_PATH: G5_BBS_PATH).'/newwin.inc.php'; // 팝업레이어 (index에서만 실행)?>		
+	
 		<div class="row">
 			<div class="col-md-9<?=$st_layout_main==2? ' col-md-push-3': ''?>">
 				<?php include_once G5_THEME_PATH.'/_inc/slider.php'?>
